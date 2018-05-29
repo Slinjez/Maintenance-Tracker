408e442650a3c2ac0b17eb3ffacaadf76d42f835
Mwangi Thiga <mwangiwathiga@gmail.com>
2018-05-29 22:41:13 +0300

    [updates 157934947] fixed test functions as recommended by my LFA

---
+++ app/tests/test_GetAllUserRequests.py
@@ -0,0 +1,21 @@
+'''the tests should cover 
+1. when user has no requests
+2. test for request without user id
+'''
+import unittest
+
+class TestGetAllUserRequests(unittest.TestCase):
+    
+    def test_client_with_no_request(self):
+        result = getAllRequest(2)
+        self.assertEqual(result,"You have not created any requests yet")
+ 
+    def test_test_user_request_without_client_id(self):
+        result = getAllRequest()
+        self.assertEqual(result,"Cannot fetch results without user id")
+    
+    
+    
+
+if __name__ == '__main__':
+    unittest.main()
