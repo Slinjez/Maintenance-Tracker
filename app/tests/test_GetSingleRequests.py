e44b33aba3d3a8b1f171b94c6018fd38fcda7dda
Mwangi Thiga <mwangiwathiga@gmail.com>
2018-05-29 19:45:05 +0300

    fixed the directory structure

---
+++ app/tests/test_GetSingleRequests.py
@@ -0,0 +1,18 @@
+'''the tests should cover 
+1. when the request is missing
+1. when request id is invalid
+'''
+import unittest
+
+class TestGetSingleUserRequest(unittest.TestCase):
+    
+    def test_testMissingRequest(self):
+        pass
+    
+    def test_testInvalidId(self):
+        pass
+    
+    
+
+if __name__ == '__main__':
+    unittest.main()
