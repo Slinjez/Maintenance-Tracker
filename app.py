594c2d3953cff91bfddc4d4aa2fc8ecd4e90eddf
Mwangi Thiga <mwangiwathiga@gmail.com>
2018-05-31 01:20:02 +0300

    [updated #157984415] last thing I can possibly do this evening

--- app.py
+++ app.py
@@ -54,33 +54,37 @@ def index():
     return "will be back with the ui soon"
 
 
-@app.route('/api/v1/signup', methods=['POST'])
+@app.route('/api/v1/users/signup', methods=['POST'])
 def signup():
     pass
 
 
-@app.route('/api/v1/login', methods=['POST'])
+@app.route('/api/v1/users/login', methods=['POST'])
 def login():
     pass
 
 #all requests belonging to a user
+
+
 @app.route('/api/v1/users/requests/<string:userid>', methods=['GET'])
 def getAllRequests(userid):
-    if not userid or userid==None:
-        userid=0
+    if not userid or userid == None:
+        userid = 0
     try:
-        if userid is None or isinstance(int(userid),int)==False:
-            response = jsonify({"requests": "You have entered an invalid user id"})
+        if userid is None or isinstance(int(userid), int) == False:
+            response = jsonify(
+                {"requests": "You have entered an invalid user id"})
             response.status_code = 200
             return response
         else:
             userid = int(userid)
     except:
         response = jsonify({"requests": "You have entered an invalid user id"})
-        response.status_code = 405 #Method not allowed
+        response.status_code = 405  # Method not allowed
         return response
 
-    theRequests = [request for request in requests if request["requestorid"] == userid]
+    theRequests = [
+        request for request in requests if request["requestorid"] == userid]
 
     if not theRequests:
         response = jsonify({"requests": "No requests for this user"})
@@ -92,23 +96,28 @@ def getAllRequests(userid):
         return response
 
 #all request with given id
+
+
 @app.route('/api/v1/users/requestbyrequestid/<string:requestid>', methods=['GET'])
 def getSingleRequest(requestid):
-    if not requestid or requestid==None:
-        requestid=0
+    if not requestid or requestid == None:
+        requestid = 0
     try:
-        if requestid is None or isinstance(int(requestid),int)==False:
-            response = jsonify({"requests": "You have entered an invalid reqest id"})
+        if requestid is None or isinstance(int(requestid), int) == False:
+            response = jsonify(
+                {"requests": "You have entered an invalid reqest id"})
             response.status_code = 404
             return response
         else:
             requestid = int(requestid)
     except:
-        response = jsonify({"requests": "You have entered an invalid request id"})
-        response.status_code = 405 #Method not allowed
+        response = jsonify(
+            {"requests": "You have entered an invalid request id"})
+        response.status_code = 405  # Method not allowed
         return response
 
-    theRequests = [request for request in requests if request["requestid"] == requestid]
+    theRequests = [
+        request for request in requests if request["requestid"] == requestid]
 
     if not theRequests:
         response = jsonify({"requests": "No requests for this request id"})
@@ -120,10 +129,12 @@ def getSingleRequest(requestid):
         return response
 
 #just all requests no order
+
+
 @app.route('/api/v1/users/requests', methods=['GET'])
 def getAllUnfilteredRequest():
-    
-    theRequests = [request for request in requests ]
+
+    theRequests = [request for request in requests]
 
     if not theRequests:
         response = jsonify({"requests": "No requests os for now"})
@@ -133,6 +144,23 @@ def getAllUnfilteredRequest():
         response = jsonify({"requests": theRequests})
         response.status_code = 200
         return response
+
+#add request
+
+
+@app.route('/api/v1/users/requests', methods=['POST'])
+def createNewRequest():
+    newrequest = {
+        'requestid': newrequest.json['requestid'],
+        'requestorid': newrequest.json['requestorid'],
+        'requesttitle': 'An Awesome Title',
+        'requestdescription': 'This is my request description',
+        'requestcreationdate': '30 may 2018',
+        'requeststatus': 1
+    }
+
+    requests.append(newrequest)
+    return jsonify({"allrequests": requests})
 
 
 if __name__ == '__main__':
