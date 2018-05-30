d4bd22b23f2a448bbbbe55dfc2e008fdc6a3257a
Mwangi Thiga <mwangiwathiga@gmail.com>
2018-05-30 23:27:57 +0300

    [updated #157935492] updated the getonerequest test to follow naming conventions

--- tests/test_GetSingleRequests.py
+++ tests/test_GetSingleRequests.py
@@ -10,7 +10,7 @@ import sys
 class TestGetSingleUserRequest(unittest.TestCase):
     
     def test_missing_request(self):
-        result = requests.get('http://127.0.0.1:5000/maintenance_tracker/api/v1/getSingleRequest/3')
+        result = requests.get('http://127.0.0.1:5000/api/v1/getSingleRequest/3')
         self.assertEqual(
             result.json(), {"requests": "No requests for this user"})
     
