998b5b46087e9d07aa4d7e4d0f68f061ec6123ee
Mwangi Thiga <mwangiwathiga@gmail.com>
2018-06-01 02:05:15 +0300

    [updates #157935492] updates get single request endpoint

--- app/tests/test_GetSingleRequests.py
+++ app/tests/test_GetSingleRequests.py
@@ -6,12 +6,18 @@ import requests
 import json
 import sys
 
+
 class TestGetSingleUserRequest(unittest.TestCase):
+    requestoutarange = {
+        "requestid": 8
+    }
     
-    def test_missing_request(self):
-        result = requests.get('http://127.0.0.1:5000/api/v1/users/request/3')
-        self.assertEqual(
-            result.json(), {"requests": "No requests for this user"})
+    def test_missing_request(self):        
+        headers = {'content-type': 'application/json'}
+        result = requests.get('http://127.0.0.1:5000/api/v1/users/request/',
+                               data=json.dumps(self.requestoutarange), headers=headers)
+        #assert result.status_code == 200
+        self.assertEqual(result.json(), {"response": "No requests for this user"})
     
     
     
