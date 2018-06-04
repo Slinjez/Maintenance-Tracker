d2a89ab56fe2455babba2945d46531e94b15475c
Mwangi Thiga <mwangiwathiga@gmail.com>
2018-06-04 15:47:41 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -323,5 +323,5 @@ def updateRequest(requestid):
             response.status_code = 200
             return response
 if __name__ == '__main__':
-    port=int(os.getenv.get("PORT", 5000))
+    port=int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0",port=port)
