48ea6db59d252acd2dc49e8db570605aee1fbd7a
Mwangi Thiga <mwangiwathiga@gmail.com>
2018-06-06 12:53:25 +0300

    [updates #158138675] implement hash ps and unique user id

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+#This file initializes in the application and brings together all of the various components.
+import os
+from flask import Flask
+app = Flask(__name__)
+
+app.config['SECRET_KEY']='thisissecret'
+
+# Import the application routes
+from app import views
