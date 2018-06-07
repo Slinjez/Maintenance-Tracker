cf0721c0e48caea3bedcbca880104db30b259685
Mwangi Thiga <mwangiwathiga@gmail.com>
2018-06-07 23:53:48 +0300

    [updates develop] set base for test create requests

---
+++ tests/test_CreateReq.py
@@ -0,0 +1,10 @@
+from app import app
+import unittest
+from flask import request
+import json
+import sys
+
+class TestSignup(unittest.TestCase):
+    def setup(self):
+        #app=flask.Flask(__name__)
+        pass
