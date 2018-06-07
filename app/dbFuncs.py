33dcaa6c3c88ffd98d48b29e2af4f0e5c580b936
Mwangi Thiga <mwangiwathiga@gmail.com>
2018-06-07 21:18:56 +0300

    [updates #158180703]created user login with db

--- app/dbFuncs.py
+++ app/dbFuncs.py
@@ -1,14 +1,56 @@
 import psycopg2
+import psycopg2.extras
 
 class dbOperations():
     connection = psycopg2.connect("dbname='maintenancetracker' user='postgres' host='localhost' password='admin'")
     cursor = connection.cursor()
 
+    def confirmNewUser(self,usermail):        
+        query="select * from users where useremail='{usermail}'".format(usermail=usermail)
+        theResult=self.getFromDb(query)
+        if not theResult:
+            return True
+        else:
+            return False
+
     def createUser(self,user):
+        username=user['username']
+        useremail=user['useremail']
+        userpassword=user['userpassword']
+        userrole=user['userrole']
+        query="insert into users (username,useremail,password,userrole) values('{username}','{useremail}','{userpassword}','{userrole}')".format(
+            username=username,
+            useremail=useremail,
+            userpassword=userpassword,
+            userrole=userrole
+        )
+        self.addToDb(query)
+        
+    
+    def confirmLogin(self,usermail):
         pass
     
-    def dbmodel.confirmLogin(usermail):
+    def getLoginCredentials(self,usermail):
         pass
-    
-    def getLoginCredentials(usermail):
-        pass
+
+    def getFromDb(self,query):
+        self.connection = psycopg2.connect("dbname='maintenancetracker' user='postgres' host='localhost' password='admin'")
+        cur = self.connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cur.execute (query)
+        resultset =cur.fetchall()
+        dict_result = []
+        for row in resultset:
+            dict_result.append(dict(row))
+        return dict_result
+        self.connection.commit()
+        self.cursor.close()
+        self.cursor.close()
+
+    def addToDb(self,query):
+        self.connection = psycopg2.connect("dbname='maintenancetracker' user='postgres' host='localhost' password='admin'")
+        self.cursor = self.connection.cursor()
+        self.cursor.execute(query)
+        self.connection.commit()
+        self.cursor.close()
+        self.cursor.close()
+
