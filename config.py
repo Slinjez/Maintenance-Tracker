e1495c6fce1cc616a80425325453855bd98f779e
Mwangi Thiga <mwangiwathiga@gmail.com>
2018-06-12 14:27:16 +0300

    [updates develop] some fix for heroku

--- config.py
+++ config.py
@@ -8,21 +8,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 connection = psycopg2.connect("dbname='maintenancetracker' user='postgres' host='localhost' password='admin'")
 cursor = connection.cursor()
 
-#create tables if non exists
-	
 
-# cursor.execute("""
-# SELECT EXISTS 
-# (
-# 	SELECT 1 
-# 	FROM pg_tables
-# 	WHERE tablename = 'users'
-# 	AND tablename = 'requests'
-# );
-# """)
-# connection.commit()
-# tableexist= cursor.fetchall()
-# print(tableexist)
 hashedpassword = generate_password_hash('admin', method='sha256')
 cursor.execute("CREATE TABLE IF NOT EXISTS users(userid SERIAL PRIMARY KEY, username varchar(20), useremail varchar(40), password text, userrole int);")
 cursor.execute("CREATE TABLE IF NOT EXISTS requests(requestid SERIAL PRIMARY KEY,requestorid int, requesttitle text, requestdescription text, requesttype int,requestdate timestamp,requeststatus int);")
@@ -33,7 +19,7 @@ defaultuser= cursor.fetchone()
 if not defaultuser:
     newuserid=str(uuid.uuid4())
     cursor.execute("INSERT INTO users(username,useremail, password,userrole) VALUES ('default','defadmin@maintrack.com', '{ps}',1);".format(ps=hashedpassword))
-    #connection.commit()
+    connection.commit()
 
 users=cursor.execute("SELECT useremail FROM users;")
 
@@ -42,5 +28,4 @@ defaultuser= cursor.fetchone()
 
 print("db setup done")
 connection.commit()
-cursor.close()
 cursor.close()
