7291e8425801a864bd069bfc39a71cb648e5a8f6
Mwangi Thiga <mwangiwathiga@gmail.com>
2018-06-13 12:08:00 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,24 @@ import psycopg2
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
-connection = psycopg2.connect("dbname='d7ald91h9iu1kq' user='fhxfwhyrjjqvwc' host='postgres://fhxfwhyrjjqvwc:a566f768bde497878d4487b3f7d6fbdf4e881ebf8df2efa5402a0a1009ecbb7c@ec2-54-243-137-182.compute-1.amazonaws.com:5432/d7ald91h9iu1kq
-' password='a566f768bde497878d4487b3f7d6fbdf4e881ebf8df2efa5402a0a1009ecbb7c'")
+connection = psycopg2.connect("dbname='maintenancetracker' user='postgres' host='localhost' password='admin'")
 cursor = connection.cursor()
 
+#create tables if non exists
+	
 
+# cursor.execute("""
+# SELECT EXISTS 
+# (
+# 	SELECT 1 
+# 	FROM pg_tables
+# 	WHERE tablename = 'users'
+# 	AND tablename = 'requests'
+# );
+# """)
+# connection.commit()
+# tableexist= cursor.fetchall()
+# print(tableexist)
 hashedpassword = generate_password_hash('admin', method='sha256')
 cursor.execute("CREATE TABLE IF NOT EXISTS users(userid SERIAL PRIMARY KEY, username varchar(20), useremail varchar(40), password text, userrole int);")
 cursor.execute("CREATE TABLE IF NOT EXISTS requests(requestid SERIAL PRIMARY KEY,requestorid int, requesttitle text, requestdescription text, requesttype int,requestdate timestamp,requeststatus int);")
@@ -20,7 +33,7 @@ defaultuser= cursor.fetchone()
 if not defaultuser:
     newuserid=str(uuid.uuid4())
     cursor.execute("INSERT INTO users(username,useremail, password,userrole) VALUES ('default','defadmin@maintrack.com', '{ps}',1);".format(ps=hashedpassword))
-    connection.commit()
+    #connection.commit()
 
 users=cursor.execute("SELECT useremail FROM users;")
 
@@ -29,4 +42,5 @@ defaultuser= cursor.fetchone()
 
 print("db setup done")
 connection.commit()
+cursor.close()
 cursor.close()
